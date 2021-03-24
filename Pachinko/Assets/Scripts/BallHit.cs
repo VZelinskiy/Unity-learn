3aebe36e8fe3b7319bba334953447bc2d40e753e
VZelinskiy <voldimar93@gmail.com>
2021-03-24 15:29:23 +0200

    add pachenko project

---
+++ Pachinko/Assets/Scripts/BallHit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BallHit : MonoBehaviour
+{
+    public string BowlingBallTag;
+    public string BouncingBallTag;
+    public GameManager GamaManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(BowlingBallTag))
+        {
+            GamaManager.UpdateBowlingScore();
+        }
+
+        if (collision.gameObject.CompareTag(BouncingBallTag))
+        {
+            GamaManager.UpdateBouncingScore();
+        }
+        
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Pachinko/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject BouncingBall;
+    public GameObject BowlingBall;
+    public Text ScoreBowling;
+    public Text ScoreBouncing;
+
+    private int BowlingBallsCount;
+    private int BouncingBallsCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreBowling.text = "0";
+        ScoreBouncing.text = "0";
+
+        BowlingBallsCount = 0;
+        BouncingBallsCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LaunchBall();
+    }
+
+    public void UpdateBowlingScore()
+    {
+        BowlingBallsCount += 1;
+        ScoreBowling.text = BowlingBallsCount.ToString();
+    }
+
+    public void UpdateBouncingScore()
+    {
+        BouncingBallsCount += 1;
+        ScoreBouncing.text = BouncingBallsCount.ToString();
+    }
+
+    private void LaunchBall()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector2 bouncingBallPosition = new Vector2(Random.Range(-8, 8), 4.2f);
+            Instantiate(BouncingBall, bouncingBallPosition, Quaternion.identity);
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Vector2 bowlingBallPosition = new Vector2(Random.Range(-8, 8), 4.2f);
+            Instantiate(BowlingBall, bowlingBallPosition, Quaternion.identity);
+        }
+    }
+}
